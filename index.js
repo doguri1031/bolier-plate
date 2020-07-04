fbe25dcf27e7cda5055bc73570553b7a18182810
lee <doguri1031@gmail.com>
2020-07-04 18:53:58 +0900

    add login api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const { User } = require("./models/User");
 
 const config = require("./config/dev");
@@ -20,12 +21,41 @@ mongoose
   .catch((err) => console.log(err));
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", (req, res) => res.send("hello world"));
+
 app.post("/register", (req, res) => {
   const user = new User(req.body);
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "not found User",
+      });
+    }
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "Not matched password",
+        });
+      }
+      user.generateToken((err, user) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+        res.cookie("x_auth", user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
+        });
+      });
     });
   });
 });
