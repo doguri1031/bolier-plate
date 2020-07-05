db506fb0f8e9e1ffc95b9b8154e2f3ef2a0589a9
lee <doguri1031@gmail.com>
2020-07-06 01:35:46 +0900

    add React & React Router

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import RandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+export default App;
